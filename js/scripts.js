8bbcf0aa51c4c05f5a654e7e6c2784b500e04990
David Khafaji <49997043+DavidKhafaji@users.noreply.github.com>
2021-11-28 23:28:15 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_jGPPbQCqK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1no346js46gbqq2ifpq6k6m1s7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
 /*!
 * Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
 * Copyright 2013-2021 Start Bootstrap
